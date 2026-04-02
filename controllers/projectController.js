da7b71afc29c8382c7955a8608aa8c7fcb4697e9
imane imziline <imaneimzline2@gmail.com>
2026-04-02 12:09:05 +0100

    update project (project-owner only)

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,19 +1,20 @@
-import Project from '../models/Project.js';
+import projet from "../models/Project.js";
+import Project from "../models/Project.js";
 
 export const createProject = async (req, res) => {
     try {
-        const { title, description, capitalGoal, maxInvestmentPercent } = req.body;
+        const { title, description, capitalGoal, maxInvestmentPercent } =
+            req.body;
 
         const project = await Project.create({
             title,
             description,
             capitalGoal,
             maxInvestmentPercent,
-            owner: req.user._id
+            owner: req.user._id,
         });
 
         res.status(201).json(project);
-
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
@@ -27,3 +28,31 @@ export const getMyProjects = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+export const updateProject = async (req, res) => {
+    try {
+        const { id } = req.params ;
+        const { title, description, capitalGoal, maxInvestmentPercent } =
+            req.body;
+
+        const project = await projet.findById(id);
+
+        if (!project) {
+            return res.status(404).json({ message: "Project not found" });
+        }
+        if (project.status === "colsed") {
+            return res.status(403).json({ message: "Project is clos" });
+        }
+
+        project.title = title;
+        project.description = description;
+        project.capitalGoal = capitalGoal;
+        project.maxInvestmentPercent = maxInvestmentPercent;
+
+        await project.save();
+        res.json(project);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,14 +2,28 @@
 // DELETE /projects/:id    → delete
 // GET    /projects/:id/investors
 
-
-import express from 'express';
-import { createProject, getMyProjects } from '../controllers/projectController.js';
-import {authMiddleware} from '../middlewares/authMiddleware.js';
-import { roleMiddleware } from '../middlewares/roleMiddleware.js'
+import express from "express";
+import {
+    createProject,
+    getMyProjects,
+    updateProject,
+} from "../controllers/projectController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { roleMiddleware } from "../middlewares/roleMiddleware.js";
 const router = express.Router();
 
-router.post('/',authMiddleware,roleMiddleware('project_owner'),createProject);
-router.get('/my', authMiddleware, getMyProjects);
+router.post(
+    "/",
+    authMiddleware,
+    roleMiddleware("project_owner"),
+    createProject,
+);
+router.get("/my", authMiddleware, getMyProjects);
+router.put(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("project_owner"),
+    updateProject,
+);
 
 export default router;
